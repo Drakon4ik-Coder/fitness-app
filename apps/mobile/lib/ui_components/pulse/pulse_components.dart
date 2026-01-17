d8d3345e060aaac0cb3408641100ec09c8a8ae64
Illia <illiadovho@gmail.com>
2026-01-17 20:45:01 +0000

    mobile: Add Pulse UI components

---
+++ apps/mobile/lib/ui_components/pulse/pulse_components.dart
@@ -0,0 +1,5 @@
+export 'glass_meal_card.dart';
+export 'glass_search_bar.dart';
+export 'glowing_progress_ring.dart';
+export 'macro_ring.dart';
+export 'neon_pill_button.dart';
--- apps/mobile/lib/ui_components/ui_components.dart
+++ apps/mobile/lib/ui_components/ui_components.dart
@@ -3,3 +3,4 @@ export 'app_primary_button.dart';
 export 'app_scaffold.dart';
 export 'app_section.dart';
 export 'inline_banner.dart';
+export 'pulse/pulse_components.dart';
